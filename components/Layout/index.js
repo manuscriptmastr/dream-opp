d6301e948c2c611edc3ed16ed788062cde67898f
Joshua Martin <joshua@bluefishds.com>
2018-05-19 16:30:34 -0400

    Add Provider/Router/Layout components

---
+++ components/Layout/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+let Layout = () =>
+  <View>
+    <Text>Open up App.js to start working on your app!</Text>
+    <Text>Changes you make will automatically reload.</Text>
+    <Text>Shake your phone to open the developer menu.</Text>
+  </View>
+
+export default Layout;
---
+++ components/Router/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Layout from '../Layout/index';
+
+let Router = () =>
+  <Layout />
+
+export default Router;
