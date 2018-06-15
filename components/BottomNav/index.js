5149ea01b674996b73f3e94a61a23abd9c1dc7f6
Joshua Martin <joshua@bluefishds.com>
2018-06-15 13:36:09 -0400

    Add icons to bottom tab navigation

--- components/BottomNav/index.js
+++ components/BottomNav/index.js
@@ -1,8 +1,41 @@
+import React from 'react';
 import { createBottomTabNavigator } from 'react-navigation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import HomeStack from '../stacks/Home/index';
 import DiscoverStack from '../stacks/Discover/index';
 import DreamStack from '../stacks/Dream/index';
 import PlanStack from '../stacks/Plan/index';
+
+let getIcon = (navigation, focused) => {
+  let lib = {
+    Home: {
+      name: 'home',
+      focused: '',
+      unfocused: '-outline'
+    },
+    Discover: {
+      name: 'search',
+      focused: '',
+      unfocused: ''
+    },
+    Dream: {
+      name: 'cloud',
+      focused: '',
+      unfocused: '-outline'
+    },
+    Plan: {
+      name: 'list',
+      focused: '-box',
+      unfocused: ''
+    }
+  }
+  let route = navigation.state.routeName;
+  let icon = lib[route];
+  let prefix = 'ios-';
+  let infix = icon.name;
+  let suffix = focused ? icon.focused : icon.unfocused;
+  return `${prefix}${infix}${suffix}`;
+};
 
 let BottomNav = createBottomTabNavigator(
   {
@@ -10,6 +43,12 @@ let BottomNav = createBottomTabNavigator(
     Discover: DiscoverStack,
     Dream: DreamStack,
     Plan: PlanStack
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) =>
+        <Ionicons name={getIcon(navigation, focused)} size={25} color={tintColor} />
+    })
   }
 );
 
