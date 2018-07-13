a8e48e4af227d259277c69abcadab5022480834c
Joshua Martin <joshua@bluefishds.com>
2018-07-13 15:11:46 -0400

    Eradicate Redux in favor of local state

--- components/Store/index.js
+++ components/Store/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Navigator from '../navigation/Navigator';
-import store from '../../store';
-import client from '../../store/api';
-import { Provider } from 'react-redux';
 import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
+
+let client = new ApolloClient({
+  uri: "http://0.tcp.ngrok.io:12186/graphql"
+});
 
 let Store = () =>
   <ApolloProvider client={client} >
-    <Provider store={store} >
-      <Navigator />
-    </Provider>
+    <Navigator />
   </ApolloProvider>
 
 export default Store;
