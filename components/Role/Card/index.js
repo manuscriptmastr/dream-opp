e3c9e34071639a0a085c51d128f0e414b8ded67d
Joshua Martin <joshua@bluefishds.com>
2018-07-19 15:27:45 -0400

    User can update role text

--- components/Role/Card/index.js
+++ components/Role/Card/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import styles from './styles';
+import { compose, withHandlers } from 'recompose';
+import { graphql } from 'react-apollo';
+import { gql } from 'apollo-boost';
 import { View, TextInput } from 'react-native';
 
-let RoleCard = ({ role }) =>
+let RoleCard = ({ role, updateRole }) =>
   <View style={styles.role}>
     <TextInput
       style={styles.roleHeader}
       returnKeyType="done"
+      onEndEditing={updateRole}
     >{role.title}</TextInput>
   </View>
 
-export default RoleCard;
+let update = gql`
+mutation UpdateRole($id: ID!, $input: RoleInput!) {
+  updateRole(id: $id, input: $input) {
+    title
+    id
+  }
+}
+`;
+
+let enhance = compose (
+  graphql(update),
+  withHandlers({
+    updateRole: ({ mutate, role }) => (e) =>
+      mutate({ variables: { id: role.id, input: { title: e.nativeEvent.text }} })
+  })
+);
+
+export default enhance(RoleCard);
