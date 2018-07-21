e66430b1cd6a3d3b6f62e185bdeb9760ceb0cf40
Joshua Martin <joshua@bluefishds.com>
2018-07-21 11:44:05 -0400

    Add destroy mutation

--- components/Role/Card/index.js
+++ components/Role/Card/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import styles from './styles';
 import { compose, withHandlers } from 'recompose';
+import { withApollo } from 'react-apollo'
 import { graphql } from 'react-apollo';
 import { gql } from 'apollo-boost';
-import { View, TextInput } from 'react-native';
+import { View, TextInput, Button } from 'react-native';
 
-let RoleCard = ({ role, updateRole }) =>
+let RoleCard = ({ role, updateRole, destroyRole }) =>
   <View style={styles.role}>
     <TextInput
       style={styles.roleHeader}
       returnKeyType="done"
       onEndEditing={updateRole}
     >{role.title}</TextInput>
+    <Button title="Delete" onPress={destroyRole} />
   </View>
 
-let update = gql`
+const UPDATE = gql`
 mutation UpdateRole($id: ID!, $input: RoleInput!) {
   updateRole(id: $id, input: $input) {
     title
@@ -23,11 +25,33 @@ mutation UpdateRole($id: ID!, $input: RoleInput!) {
 }
 `;
 
+const DESTROY = gql`
+mutation DestroyRole($id: ID!) {
+  destroyRole(id: $id) {
+    title
+    id
+  }
+}
+`;
+
 let enhance = compose (
-  graphql(update, { name: 'update' }),
+  withApollo,
+  graphql(UPDATE, { name: 'update' }),
+  graphql(DESTROY, { name: 'destroy' }),
   withHandlers({
-    updateRole: ({ update, role }) => (e) =>
-      update({ variables: { id: role.id, input: { title: e.nativeEvent.text }} })
+    updateRole: ({ update, role: { id } }) => (e) =>
+      update({
+        variables: {
+          id,
+          input: {
+            title: e.nativeEvent.text
+          }
+        }
+      }),
+    destroyRole: ({ destroy, role: { id }, client }) => () => {
+      destroy({ variables: { id } });
+      client.resetStore();
+    }
   })
 );
 
