cd79e735cb94bfb8e2a39d621a2cab2d66144e42
Joshua Martin <joshua@bluefishds.com>
2018-07-25 14:16:01 -0400

    AddRole refactored

--- components/Role/Card/index.js
+++ components/Role/Card/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './styles';
-import { compose, withHandlers } from 'recompose';
-import { graphql, withApollo } from 'react-apollo';
+import { compose } from 'recompose';
+import { graphql } from 'react-apollo';
 import { UpdateRole, DestroyRole } from '../../../graphql/role.gql'
 import { View, TextInput, Button } from 'react-native';
 
@@ -10,30 +10,41 @@ let RoleCard = ({ role, updateRole, destroyRole }) =>
     <TextInput
       style={styles.roleHeader}
       returnKeyType="done"
-      onEndEditing={updateRole}
+      onEndEditing={(e) => updateRole({ title: e.nativeEvent.text })}
     >{role.title}</TextInput>
-    <Button title="Delete" onPress={destroyRole} />
+    {/* <Button title="Delete" onPress={destroyRole} /> */}
   </View>
 
 let enhance = compose (
-  withApollo,
-  graphql(UpdateRole, { name: 'update' }),
-  graphql(DestroyRole, { name: 'destroy' }),
-  withHandlers({
-    updateRole: ({ update, role: { id } }) => (e) =>
-      update({
+  graphql(UpdateRole, {
+    name: 'updateRole',
+    props: ({ updateRole, ownProps: { role: { id } } }) => ({
+      updateRole: (input) => updateRole({
         variables: {
           id,
-          input: {
-            title: e.nativeEvent.text
-          }
+          input
         }
-      }),
-    destroyRole: ({ destroy, role: { id }, client }) => () => {
-      destroy({ variables: { id } });
-      client.resetStore();
-    }
-  })
+      })
+    })
+  }),
+  // graphql(DestroyRole, {
+  //   name: 'destroyRole'
+  // }),
+  // withHandlers({
+  //   updateRole: ({ update, role: { id } }) => (e) =>
+  //     update({
+  //       variables: {
+  //         id,
+  //         input: {
+  //           title: e.nativeEvent.text
+  //         }
+  //       }
+  //     }),
+  //   destroyRole: ({ destroy, role: { id }, client }) => () => {
+  //     destroy({ variables: { id } });
+  //     client.resetStore();
+  //   }
+  // })
 );
 
 export default enhance(RoleCard);
