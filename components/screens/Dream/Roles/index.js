2b7660fef60686bc24f013bcf92fef4a4063108f
Joshua Martin <joshua@bluefishds.com>
2018-07-25 13:32:34 -0400

    Refactor role query add add composefunctions

--- components/screens/Dream/Roles/index.js
+++ components/screens/Dream/Roles/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { compose, mapProps, branch, renderNothing, withHandlers } from 'recompose';
-import { graphql, withApollo } from 'react-apollo';
+import { compose, branch, renderNothing } from 'recompose';
+import { graphql } from 'react-apollo';
 import { ReadRoles, CreateRole } from '../../../../graphql/role.gql';
 import { TouchableOpacity } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -9,7 +9,7 @@ import RoleList from '../../../Role/List';
 
 let RolesScreen = ({ roles, createRole }) =>
   <ScreenView>
-    <TouchableOpacity onPress={createRole}>
+    <TouchableOpacity onPress={() => createRole()}>
       <Ionicons name="ios-add" size={25} color="blue" />
     </TouchableOpacity>
     <RoleList roles={roles} />
@@ -18,23 +18,27 @@ let RolesScreen = ({ roles, createRole }) =>
 let nothingWhileLoading = (isLoading) => branch(isLoading, renderNothing);
 
 let enhance = compose(
-  withApollo,
-  graphql(ReadRoles),
-  graphql(CreateRole, { name: 'create' }),
-  nothingWhileLoading(({ data: { loading } }) => loading),
-  mapProps(({ data, ...props }) => ({ ...data, ...props })),
-  withHandlers({
-    createRole: ({ create, client }) => () => {
-      create({
-        variables: {
-          input: {
-            title: "Press to edit"
-          }
+  graphql(ReadRoles, {
+    props: ({ data: { loading, roles, updateQuery } }) => ({
+      loading, roles, updateQuery
+    })
+  }),
+  graphql(CreateRole, {
+    name: 'createRole',
+    options: ({ updateQuery }) => ({
+      variables: {
+        input: {
+          title: "Press to edit"
         }
-      });
-      client.resetStore();
-    }
-  })
+      },
+      update: (_, { data: { createRole } }) =>
+        updateQuery((prev) => ({
+          ...prev, roles: [ ...prev.roles, createRole ]
+        })
+      )
+    })
+  }),
+  nothingWhileLoading(({ loading }) => loading)
 );
 
 export default enhance(RolesScreen);
