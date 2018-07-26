c9d23525bfcd42dd0080800c9c064d07c48dfa8a
Joshua Martin <joshua@bluefishds.com>
2018-07-26 11:01:39 -0400

    Copy functionality from roles

--- components/Tool/Card/index.js
+++ components/Tool/Card/index.js
@@ -1,10 +1,43 @@
 import React from 'react';
 import styles from './styles';
-import { View, Text } from 'react-native';
+import { compose } from 'recompose';
+import { graphql } from 'react-apollo';
+import { UpdateTool, DestroyTool } from '../../../graphql/tool.gql'
+import { View, TextInput, Button } from 'react-native';
 
-let ToolCard = ({ tool }) =>
+let ToolCard = ({ tool, updateTool, destroyTool }) =>
   <View style={styles.tool}>
-    <Text style={styles.toolHeader}>{tool.title}</Text>
+    <TextInput
+      style={styles.toolHeader}
+      returnKeyType="done"
+      onEndEditing={(e) => updateTool({ title: e.nativeEvent.text })}
+    >{tool.title}</TextInput>
+    <Button title="Delete" onPress={() => destroyTool()} />
   </View>
 
-export default ToolCard;
+let enhance = compose (
+  graphql(UpdateTool, {
+    name: 'updateTool',
+    props: ({ updateTool, ownProps: { tool: { id } } }) => ({
+      updateTool: (input) => updateTool({
+        variables: {
+          id,
+          input
+        }
+      })
+    })
+  }),
+  graphql(DestroyTool, {
+    name: 'destroyTool',
+    options: ({ tool: { id }, updateTools }) => ({
+      variables: { id },
+      update: () =>
+        updateTools((prev) => ({
+          ...prev, tools: prev.tools.filter(tool => tool.id !== id)
+        })
+      )
+    })
+  })
+);
+
+export default enhance(ToolCard);
--- components/Tool/List/index.js
+++ components/Tool/List/index.js
@@ -3,11 +3,11 @@ import styles from './styles';
 import { ScrollView, View } from 'react-native';
 import ToolCard from '../Card';
 
-let ToolList = ({ tools }) =>
+let ToolList = ({ tools, updateTools }) =>
   <ScrollView style={styles.toolList}>
     {tools.map(tool =>
       <View key={tool.id} style={styles.toolListItem} >
-        <ToolCard tool={tool} />
+        <ToolCard tool={tool} updateTools={updateTools} />
       </View>
     )}
   </ScrollView>
